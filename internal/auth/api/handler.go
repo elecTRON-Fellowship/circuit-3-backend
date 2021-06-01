8cefc88253a737a9a8f5b64944cec03ebd55c0db
neel229 <device.neel@gmail.com>
2021-06-01 12:31:18 +0530

    Add CRUD for user-auth

--- internal/auth/api/handler.go
+++ internal/auth/api/handler.go
@@ -44,7 +44,7 @@ func (s *Server) RegisterUser(ctx *fiber.Ctx) error {
 // GetUser fetches a user's data given his id
 func (s *Server) GetUser(ctx *fiber.Ctx) error {
 	// get the id from query params
-	key := ctx.Query("id")
+	key := ctx.Params("id")
 	// convert the id from string to int64
 	id, err := strconv.Atoi(key)
 	if err != nil {
@@ -59,4 +59,232 @@ func (s *Server) GetUser(ctx *fiber.Ctx) error {
 	}
 
 	return ctx.Status(fiber.StatusOK).JSON(user)
+}
+
+// GetUserByEmail fetches a user's details based on the email provided
+func (s *Server) GetUserByEmail(ctx *fiber.Ctx) error {
+	key := ctx.Params("email")
+	user, err := s.repo.GetUserByEmail(ctx.Context(), key)
+	if err != nil {
+		log.Fatal(err)
+		return fiber.ErrInternalServerError
+	}
+	return ctx.Status(fiber.StatusOK).JSON(user)
+}
+
+// GetUserByUserName fetches a user's details based on the user_name provided
+func (s *Server) GetUserByUserName(ctx *fiber.Ctx) error {
+	key := ctx.Params("username")
+	user, err := s.repo.GetUserByUserName(ctx.Context(), key)
+	if err != nil {
+		log.Fatal(err)
+		return fiber.ErrInternalServerError
+	}
+	return ctx.Status(fiber.StatusOK).JSON(user)
+}
+
+// UpdateFirstName updates firstname of the user given his id
+func (s *Server) UpdateFirstName(ctx *fiber.Ctx) error {
+	data := new(user)
+	if err := ctx.BodyParser(&data); err != nil {
+		log.Fatal(err)
+		return fiber.ErrBadRequest
+	}
+
+	// get the id from query params
+	key := ctx.Params("id")
+	// convert the id from string to int64
+	id, err := strconv.Atoi(key)
+	if err != nil {
+		log.Fatal(err)
+		return fiber.ErrBadRequest
+	}
+
+	if err = s.repo.UpdateFirstName(ctx.Context(), db.UpdateFirstNameParams{
+		ID:        int64(id),
+		FirstName: data.FirstName,
+	}); err != nil {
+		log.Fatal(err)
+		return fiber.ErrInternalServerError
+	}
+	return ctx.Status(fiber.StatusOK).JSON("FirstName updated successfully...")
+}
+
+// UpdateLastName updates lastname of the user given his id
+func (s *Server) UpdateLastName(ctx *fiber.Ctx) error {
+	data := new(user)
+	if err := ctx.BodyParser(&data); err != nil {
+		log.Fatal(err)
+		return fiber.ErrBadRequest
+	}
+
+	// get the id from query params
+	key := ctx.Params("id")
+	// convert the id from string to int64
+	id, err := strconv.Atoi(key)
+	if err != nil {
+		log.Fatal(err)
+		return fiber.ErrBadRequest
+	}
+
+	if err = s.repo.UpdateLastName(ctx.Context(), db.UpdateLastNameParams{
+		ID:       int64(id),
+		LastName: data.LastName,
+	}); err != nil {
+		log.Fatal(err)
+		return fiber.ErrInternalServerError
+	}
+	return ctx.Status(fiber.StatusOK).JSON("LastName updated successfully...")
+}
+
+// UpdateUserName updates username of the user given his id
+func (s *Server) UpdateUserName(ctx *fiber.Ctx) error {
+	data := new(user)
+	if err := ctx.BodyParser(&data); err != nil {
+		log.Fatal(err)
+		return fiber.ErrBadRequest
+	}
+
+	// get the id from query params
+	key := ctx.Params("id")
+	// convert the id from string to int64
+	id, err := strconv.Atoi(key)
+	if err != nil {
+		log.Fatal(err)
+		return fiber.ErrBadRequest
+	}
+
+	if err = s.repo.UpdateUserName(ctx.Context(), db.UpdateUserNameParams{
+		ID:       int64(id),
+		UserName: data.UserName,
+	}); err != nil {
+		log.Fatal(err)
+		return fiber.ErrInternalServerError
+	}
+	return ctx.Status(fiber.StatusOK).JSON("UserName updated successfully...")
+}
+
+// UpdateEmail updates email of the user given his id
+func (s *Server) UpdateEmail(ctx *fiber.Ctx) error {
+	data := new(user)
+	if err := ctx.BodyParser(&data); err != nil {
+		log.Fatal(err)
+		return fiber.ErrBadRequest
+	}
+
+	// get the id from query params
+	key := ctx.Params("id")
+	// convert the id from string to int64
+	id, err := strconv.Atoi(key)
+	if err != nil {
+		log.Fatal(err)
+		return fiber.ErrBadRequest
+	}
+
+	if err = s.repo.UpdateEmail(ctx.Context(), db.UpdateEmailParams{
+		ID:    int64(id),
+		Email: data.Email,
+	}); err != nil {
+		log.Fatal(err)
+		return fiber.ErrInternalServerError
+	}
+	return ctx.Status(fiber.StatusOK).JSON("Email updated successfully...")
+}
+
+// UpdatePassword updates password of the user given his id
+func (s *Server) UpdatePassword(ctx *fiber.Ctx) error {
+	data := new(user)
+	if err := ctx.BodyParser(&data); err != nil {
+		log.Fatal(err)
+		return fiber.ErrBadRequest
+	}
+
+	// get the id from query params
+	key := ctx.Params("id")
+	// convert the id from string to int64
+	id, err := strconv.Atoi(key)
+	if err != nil {
+		log.Fatal(err)
+		return fiber.ErrBadRequest
+	}
+
+	if err = s.repo.UpdatePassword(ctx.Context(), db.UpdatePasswordParams{
+		ID:       int64(id),
+		Password: data.Password,
+	}); err != nil {
+		log.Fatal(err)
+		return fiber.ErrInternalServerError
+	}
+	return ctx.Status(fiber.StatusOK).JSON("Password updated successfully...")
+}
+
+// UpdatePhoneNo updates phone number of the user given his id
+func (s *Server) UpdatePhoneNo(ctx *fiber.Ctx) error {
+	data := new(user)
+	if err := ctx.BodyParser(&data); err != nil {
+		log.Fatal(err)
+		return fiber.ErrBadRequest
+	}
+
+	// get the id from query params
+	key := ctx.Params("id")
+	// convert the id from string to int64
+	id, err := strconv.Atoi(key)
+	if err != nil {
+		log.Fatal(err)
+		return fiber.ErrBadRequest
+	}
+
+	if err = s.repo.UpdatePhoneNo(ctx.Context(), db.UpdatePhoneNoParams{
+		ID:      int64(id),
+		PhoneNo: data.PhoneNo,
+	}); err != nil {
+		log.Fatal(err)
+		return fiber.ErrInternalServerError
+	}
+	return ctx.Status(fiber.StatusOK).JSON("Phone Number updated successfully...")
+}
+
+// ListUsers returns a list of users given the limit and offset in the url query
+func (s *Server) ListUsers(ctx *fiber.Ctx) error {
+	// get the value of limit
+	limitString := ctx.Query("limit")
+	limit, err := strconv.Atoi(limitString)
+	if err != nil {
+		log.Fatal(err)
+		return fiber.ErrBadRequest
+	}
+	// get the value of offset
+	offsetString := ctx.Query("offset")
+	offset, err := strconv.Atoi(offsetString)
+	if err != nil {
+		log.Fatal(err)
+		return fiber.ErrBadRequest
+	}
+	users, err := s.repo.ListUsers(ctx.Context(), db.ListUsersParams{
+		Limit:  int32(limit),
+		Offset: int32(offset),
+	})
+	if err != nil {
+		log.Fatal(err)
+		return fiber.ErrInternalServerError
+	}
+	return ctx.Status(fiber.StatusOK).JSON(users)
+}
+
+// DeleteUser deletes a user from the db
+func (s *Server) DeleteUser(ctx *fiber.Ctx) error {
+	// get the id from query params
+	key := ctx.Params("id")
+	// convert the id from string to int64
+	id, err := strconv.Atoi(key)
+	if err != nil {
+		log.Fatal(err)
+		return fiber.ErrBadRequest
+	}
+	if err := s.repo.DeleteUser(ctx.Context(), int64(id)); err != nil {
+		log.Fatal(err)
+		return fiber.ErrInternalServerError
+	}
+	return ctx.Status(fiber.StatusOK).JSON("User successfully deleted...")
 }
--- internal/auth/api/server.go
+++ internal/auth/api/server.go
@@ -29,9 +29,21 @@ func NewServer(repo *db.Repo) (*Server, error) {
 // Initialize the handlers to respective
 // routes
 func (s *Server) SetRoutes() error {
-	auth := s.app.Group("/api")
-	auth.Post("/register", s.RegisterUser)
-	auth.Get("/user", s.GetUser)
+	s.app.Post("/register", s.RegisterUser)
+	s.app.Get("/users", s.ListUsers)
+
+	user := s.app.Group("/user")
+	user.Get("/id/:id", s.GetUser)
+	user.Get("/email/:email", s.GetUserByEmail)
+	user.Get("/username/:username", s.GetUserByUserName)
+	user.Put("/:id/firstname", s.UpdateFirstName)
+	user.Put("/:id/lastname", s.UpdateLastName)
+	user.Put("/:id/username", s.UpdateUserName)
+	user.Put("/:id/email", s.UpdateEmail)
+	user.Put("/:id/password", s.UpdatePassword)
+	user.Put("/:id/phone", s.UpdatePhoneNo)
+	user.Delete("/:id", s.DeleteUser)
+
 	return nil
 }
 
