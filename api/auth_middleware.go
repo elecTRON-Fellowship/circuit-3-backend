350ee5af919fea8e7c8441c66282d3cae742a98c
neel229 <device.neel@gmail.com>
2021-06-02 23:58:22 +0530

    Add auth middleware

---
+++ api/auth_middleware.go
@@ -0,0 +1,48 @@
+package api
+
+import (
+	"log"
+	"strings"
+
+	"github.com/gofiber/fiber/v2"
+)
+
+const (
+	authorizationHeaderKey  = "authorization"
+	authorizationTypeBearer = "bearer"
+	authorizationPayload    = "payload"
+)
+
+func (s *Server) AuthMiddleware() func(*fiber.Ctx) error {
+	return func(c *fiber.Ctx) error {
+		authorizationHeader := c.Get(authorizationHeaderKey)
+		if len(authorizationHeader) == 0 {
+			return c.Status(fiber.StatusUnauthorized).JSON(fiber.Map{
+				"error": "authorization header is not provided",
+			})
+		}
+		fields := strings.Fields(authorizationHeader)
+		if len(fields) < 2 {
+			return c.Status(fiber.StatusUnauthorized).JSON(fiber.Map{
+				"error": "invalid authorization header format",
+			})
+		}
+		authorizationType := strings.ToLower(fields[0])
+		if authorizationType != authorizationTypeBearer {
+			return c.Status(fiber.StatusUnauthorized).JSON(fiber.Map{
+				"error": "invalid authorization type",
+			})
+		}
+		accessToken := fields[1]
+		payload, err := s.token.VerifyToken(accessToken)
+		if err != nil {
+			return c.Status(fiber.StatusUnauthorized).JSON(fiber.Map{
+				"error": err,
+			})
+		}
+
+		c.Set(authorizationPayload, payload.UserName)
+		log.Print(c.Get(authorizationPayload))
+		return c.Next()
+	}
+}
